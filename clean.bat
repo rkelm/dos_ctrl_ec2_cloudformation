91e09c0bc5d0c4aa4fa530fb43e85646e2445dc7
Robert Kelm <git@kelm.net>
2019-01-28 21:43:52 +0100

    Changed name prefixes to cf_.

--- clean.bat
+++ clean.bat
@@ -14,6 +14,8 @@ IF EXIST prepared-stack.txt DEL prepared-stack.txt
 IF EXIST dos_ctrl_ec2.log DEL dos_ctrl_ec2.log
 IF EXIST cmd_status.txt DEL cmd_status.txt
 IF EXIST commandid.txt DEL commandid.txt
+IF EXIST output.txt DEL output.txt
+IF EXIST instanceid_TEST.txt DEL instanceid_TEST.txt
 
 REM Switching back to previous directory.
 CD %EXCURRENTDIR%
