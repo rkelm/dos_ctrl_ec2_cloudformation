acd542d01a6ac749cde6ab35a62ef79e21130de0
Robert Kelm <git@kelm.net>
2019-08-02 09:48:27 +0200

    added build package batch

--- build_package.bat
+++ build_package.bat
@@ -1,31 +1,33 @@
 @ECHO OFF
 REM Batch file to create package for distribution to users.
-SET LOCAL_DIR=dos_ctrl_ec2\
-SET DSTFILENAME=build\dos_ctrl_ec2.zip
-
+SET LOCAL_DIR=dos_ctrl_ec2_cloudformation\
+SET BUILD_DIR=build\
+SET DSTFILENAME=dos_ctrl_ec2_cloudformation.zip
 REM Remember previous current directory.
 SET EXCURRENTDIR=%CD%
-REM Switch current directory to installation directory.
+REM Switch current directory to one level above batch file directory.
 CD /D %~dp0
-
-REM Remove old package if it exists.
-IF EXIST %DSTFILENAME% (
-	DEL %DSTFILENAME%
-) 
-REM Move up one directory level.
 CD ..
 
-ECHO Creating deployment package %DSTFILENAME%
-C:\Programme\7-Zip\7z.exe a %LOCAL_DIR%%DSTFILENAME% @%LOCAL_DIR%build_file_list.txt
+ECHO '************ Building %BUILD_DIR%%DSTFILENAME% ************'
+COPY %LOCAL_DIR%setup_shortcuts.bat .
+
+REM Add files to control Minecraft instance.
+C:\Programme\7-Zip\7z.exe a %LOCAL_DIR%%BUILD_DIR%%DSTFILENAME% @%LOCAL_DIR%build_file_list.txt
 
 IF ERRORLEVEL 1 (
 	ECHO Error creating deployment package.
-	) ELSE (
+	REM Restore previous current directory.
+	CD /D %EXCURRENTDIR%
+	EXIT /B 1
+) ELSE (
 	ECHO Successfully created deployment package.
-	)
-	
+)
+
+CD %LOCAL_DIR%\build
+
 REM Restore previous current directory.
 CD /D %EXCURRENTDIR%
 
+ECHO '************ Successfully created deployment package ************'
 PAUSE
-
