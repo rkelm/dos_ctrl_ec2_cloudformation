19f6197aba9a3aa7c28590e52a0f496ac6f48da5
Robert Kelm <git@kelm.net>
2019-08-02 09:52:02 +0200

    added batch to clean temporary files

--- clean.bat
+++ clean.bat
@@ -16,6 +16,15 @@ IF EXIST cmd_status.txt DEL cmd_status.txt
 IF EXIST commandid.txt DEL commandid.txt
 IF EXIST output.txt DEL output.txt
 IF EXIST instanceid_TEST.txt DEL instanceid_TEST.txt
+IF EXIST cmd_status.txt DEL cmd_status.txt
+IF EXIST defaultvpc.txt DEL defaultvpc.txt
+IF EXIST job_id.txt DEL job_id.txt
+IF EXIST map_ids.txt DEL map_ids.txt
+IF EXIST map_ids2.txt DEL map_ids2.txt
+IF EXIST prepared-stack.txt DEL prepared-stack.txt
+IF EXIST renderjobdef.txt DEL renderjobdef.txt
+IF EXIST renderjobqueue.txt DEL renderjobqueue.txt
+IF EXIST run-stack.txt DEL run-stack.txt
 
 REM Switching back to previous directory.
 CD %EXCURRENTDIR%
